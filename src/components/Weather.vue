7aabf07e7d599682268c318083bd8fac95487641
Wesley Maffly-Kipp <maffkipp@yahoo.com>
2018-05-16 17:40:12 -0500

    added api call for openWeather

--- src/components/Weather.vue
+++ src/components/Weather.vue
@@ -1,15 +1,37 @@
 <template>
     <div>
-        <form action="input">
-            <input type="text" name="location-search">
+        <form v-on:submit="loadWeather" action="input">
+            <input v-bind="cityName" type="text" name="location-search">
             <button type="submit">Search</button>
         </form>
     </div>
 </template>
 
 <script>
+/* eslint-disable */
+import axios from 'axios'
+
 export default {
     name: 'Weather',
+    data() {
+        return {
+            cityName: '',
+            key: 'f44e9d4e81fbd380f38ccb737f016c30'
+        }
+    },
+    methods: {
+        loadWeather() {
+            let vm = this;
+            console.log(`api.openweathermap.org/data/2.5/weather?q=${vm.cityName}&APPID=${vm.key}`);
+            axios.get(`api.openweathermap.org/data/2.5/weather?q=${vm.cityName}&APPID=${vm.key}`)
+            .then(response => {
+                console.log(response);
+            })
+            .catch(error => {
+                console.log(error);
+            })
+        }
+    }
 };
 </script>
 
