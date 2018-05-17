3e214b4f82e499d6103f1e5b930b717e63eb0bfc
Wesley Maffly-Kipp <maffkipp@yahoo.com>
2018-05-16 21:52:20 -0500

    app now displays weather data on search

--- src/components/Weather.vue
+++ src/components/Weather.vue
@@ -1,9 +1,13 @@
 <template>
     <div>
         <form v-on:submit="getWeatherData" action="input">
-            <input v-model="cityName" type="text" name="location-search">
+            <input v-model="citySearch" type="text" name="location-search">
             <button type="submit">Search</button>
         </form>
+        <h1>{{ weather.cityName }}</h1>
+        <h2>{{ weather.temp }}</h2>
+        <h3>{{ weather.main }}</h3>
+        <p>{{ weather.description }}</p>
     </div>
 </template>
 
@@ -15,24 +19,33 @@ export default {
     name: 'Weather',
     data() {
         return {
-            cityName: '',
+            citySearch: '',
             key: 'f44e9d4e81fbd380f38ccb737f016c30',
             weather: {
-
+                cityName: '',
+                temp: '',
+                main: '',
+                description: ''
             }
         }
     },
     methods: {
+
         getWeatherData(e) {
             e.preventDefault();
             let vm = this;
-            axios.get(`https://api.openweathermap.org/data/2.5/weather?q=${vm.cityName}&APPID=${vm.key}`)
+            axios.get(`https://api.openweathermap.org/data/2.5/weather?q=${vm.citySearch}&APPID=${vm.key}`)
             .then(response => {
-                console.log(response);
+                vm.weather.cityName = response.data.name;
+                vm.weather.temp = vm.setInitialTemp(response.data.main.temp);
+                vm.weather.main = response.data.weather[0].main;
+                vm.weather.description = response.data.weather[0].description;
             })
-            .catch(error => {
-                console.err(error);
-            })
+            .catch(error => console.err(error))
+        },
+
+        setInitialTemp(temp) {
+            return (Math.round((temp * (9/5) - 459.67) * 100) / 100);
         }
     }
 };
